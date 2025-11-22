16823815881997ab32a3b0243251b34058e5aa57
vinay <vinayboddu2003@gmail.com>
2025-11-22 11:21:29 +0530

    issue price updated in ipo.js file

--- ipo.js
+++ ipo.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-const schema = new mongoose.Schema({
-  company:{type:mongoose.Schema.Types.ObjectId, ref:'Company', required:true},
-  issueSize:Number,
-  priceBandLow:Number,
-  priceBandHigh:Number,
-  openDate:Date,
-  closeDate:Date,
-  status:{type:String,default:'upcoming'}
-});
-module.exports = mongoose.model('IPO', schema);
+const mongoose = require('mongoose');
 
+const IPOIssueSchema = new mongoose.Schema({
+  company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+  openDate: { type: Date, required: true },
+  closeDate: { type: Date, required: true },
+  priceBand: { low: Number, high: Number },
+  listingPrice: Number,
+  lotSize: { type: Number, default: 1 }
+}, { timestamps: true });
+
+module.exports = mongoose.model('IPOIssue', IPOIssueSchema);
